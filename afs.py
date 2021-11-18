d723117b46d1c6a1fa6ac6b33b3bf43394745451
Misaki Kasumi <misakikasumi@outlook.com>
2021-11-18 10:56:35 +0800

    single pass

--- afs.py
+++ afs.py
@@ -9,31 +9,6 @@ import pysubs2
 
 
 def ass_font_subset(ass_files: Iterable[os.PathLike], fonts_dir: os.PathLike, output_dir: os.PathLike) -> None:
-    # collect chars
-    def no_at(fn: str) -> str:
-        if fn[0] == "@":
-            fn = fn[1:]
-        return fn
-    ass_files = [os.fsdecode(p) for p in ass_files]
-    ass_list = [pysubs2.load(p) for p in ass_files]
-    char_map: DefaultDict[str, Set[str]] = defaultdict(set)
-    fn_reg = re.compile(r"(?<=\\fn)[^\}\\]+")
-    for ass in ass_list:
-        for ln in ass.events:
-            if ln.is_comment or ln.is_drawing:
-                continue
-            style = ass.styles[ln.style]
-            used_fonts = [no_at(style.fontname)]
-            for match in fn_reg.finditer(ln.text):
-                used_fonts.append(no_at(match.group(0)))
-            plaintext = ln.plaintext
-            for fn in used_fonts:
-                char_map[fn].update(plaintext)
-    
-    # filter char_map
-    for chars in char_map.values():
-        chars.discard("\n")
-    
     # collect fonts
     fonts_dir = os.fsdecode(fonts_dir)
     font_files: list[str] = []
@@ -70,30 +45,37 @@ def ass_font_subset(ass_files: Iterable[os.PathLike], fonts_dir: os.PathLike, ou
             font_map[new_fn][fs_selection] = font
     
     # modify subtitles
+    ass_files = [os.fsdecode(p) for p in ass_files]
+    ass_list = [pysubs2.load(p, format_="ass") for p in ass_files]
+    char_map: DefaultDict[str, Set[str]] = defaultdict(set)
+    fn_reg = re.compile(r"(?<=\\fn)[^\}\\]+")
     output_dir = os.fsdecode(output_dir)
-    def repl_fn(match):
-        fn = match if isinstance(match, str) else match.group(0)
-        new_fn = fontname_map[no_at(fn)]
-        if fn[0] == "@": new_fn = "@" + new_fn
+    def repl_fn(fn: str, no_at: bool = False) -> str:
+        new_fn = fontname_map[fn[1:] if fn[0] == "@" else fn]
+        if fn[0] == "@" and not no_at: new_fn = "@" + new_fn
         return new_fn
+    def fn_collect_and_repl(match: re.Match) -> str:
+        fn = match.group(0)
+        used_fonts.append(repl_fn(fn, True))
+        return repl_fn(fn)
     for filename, ass in zip(ass_files, ass_list):
         used_styles = set()
         for ln in ass.events:
             if ln.is_comment or ln.is_drawing:
                 continue
-            ln.text = fn_reg.sub(repl_fn, ln.text)
+            plaintext = ln.plaintext
+            style = ass.styles[ln.style]
+            used_fonts = [repl_fn(style.fontname, True)]
+            ln.text = fn_reg.sub(fn_collect_and_repl, ln.text)
+            for fn in used_fonts:
+                char_map[fn].update(plaintext)
             used_styles.add(ln.style)
         for style_name in used_styles:
             style = ass.styles[style_name]
             style.fontname = repl_fn(style.fontname)
         ass.save(os.path.join(output_dir, os.path.basename(filename)))
-    
-    # update char_map
-    new_char_map: DefaultDict[str, Set[str]] = defaultdict(set)
-    for fn, chars in char_map.items():
-        new_fn = fontname_map[fn]
-        new_char_map[new_fn].update(chars)
-    char_map = new_char_map
+    for chars in char_map.values():
+        chars.discard("\n")
     
     # subset fonts
     font_style_map = {
