9e65ee28294497d48184bc6bf5194e5473550f60
Misaki Kasumi <misakikasumi@outlook.com>
2021-11-20 02:03:02 +0800

    do not ignore missing unicodes

--- afs.py
+++ afs.py
@@ -88,7 +88,7 @@ def ass_font_subset(ass_files: Iterable[os.PathLike], fonts_dir: os.PathLike, ou
         0b1000000: "Regular",
     }
     for fn, chars in char_map.items():
-        subsetter = subset.Subsetter(subset.Options(hinting=False, layout_features=["vrt2"]))
+        subsetter = subset.Subsetter(subset.Options(hinting=False, layout_features=["vrt2"], ignore_missing_unicodes=False))
         subsetter.populate(text="".join(chars))
         for fs, font in font_map[fn].items():
             name_table = font["name"]
